dc9971ac1f84b6cb45c0b1eb2ccadc47a5b6bd56
KVA24 <khiennd98@gmail.com>
2022-07-11 14:46:16 +0700

    Updates

---
+++ precache-manifest.3aad65d8e9c245fddcd1098fdd875912.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa0fba5432a63f4c481f416294c5426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f04fcddbab743ee6561",
+    "url": "/static/css/2.00309343.chunk.css"
+  },
+  {
+    "revision": "c8abf044211118b3d335",
+    "url": "/static/css/main.8b74e5bf.chunk.css"
+  },
+  {
+    "revision": "5f04fcddbab743ee6561",
+    "url": "/static/js/2.e6ee40dc.chunk.js"
+  },
+  {
+    "revision": "cc66d763f8aaf1cee1d9856c945730fa",
+    "url": "/static/js/2.e6ee40dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8abf044211118b3d335",
+    "url": "/static/js/main.aac48d77.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.aac48d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea1e1cf0657502f2539",
+    "url": "/static/js/runtime-main.6c017fe2.js"
+  }
+]);
